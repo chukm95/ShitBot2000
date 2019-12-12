2243d9800cf27fb8808d6ef0eec294013cff6d2a
Chukm95 <jesse.m@live.nl>
2019-12-12 10:18:21 +0100

    shit

---
+++ src/Implementation/NetworkComponent.java
@@ -0,0 +1,20 @@
+package Implementation;
+
+import TI.BoeBot;
+
+import java.net.ServerSocket;
+
+public class NetworkComponent implements IComponent {
+
+
+    private ServerSocket serverSocket;
+
+    private NetworkComponent(){
+
+    }
+
+    @Override
+    public void update(double deltaTime) {
+
+    }
+}
--- src/Implementation/ShitBot.java
+++ src/Implementation/ShitBot.java
@@ -80,5 +80,4 @@ public class ShitBot {
         currentBehavior.OnActivate();
     }
 
-
 }
